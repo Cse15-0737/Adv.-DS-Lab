606e88ab84f736e7e21f58323be1c22e4f9e7c12
Cse15-0737 <aryanbrijrajrai@gmail.com>
2026-08-13 14:47:04 +0530

    Circular Queue Using Linked list

---
+++ Queue/CIrcularQueueUsingLL.cpp
@@ -0,0 +1,63 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+class Node{
+    public:
+    int data;
+    Node *next;
+    Node(int value){
+        data=value;
+        next=NULL;
+    }
+};
+class Queue{
+    public:
+    Node *rear;
+    Node *front;
+    Queue(){
+        rear=front=NULL;
+    }
+    void enqueue(int val){
+        Node *temp=new Node(val);
+        if(rear==NULL){
+            front=rear=temp;
+            return;
+        }
+        rear->next=temp;
+        rear=temp;
+    }
+    int dequeue(){
+        if(front==NULL){
+            cout<<"Queue is Empty "<<endl;
+            return -1;
+        }
+        Node *temp=front;
+        front=front->next;
+        if(front==NULL){
+            rear=NULL;
+        }
+        int x=temp->data;
+        delete temp;
+        return x;
+    }
+    void display(){
+        if(front==NULL){
+            cout<<"Queue is Empty "<<endl;
+            return;
+        }
+        Node *temp=front;
+        while(temp!=NULL){
+            cout<<temp->data<<" ";
+            temp=temp->next;
+        }
+        cout<<endl;
+    }
+};
+int main(){
+    Queue q;
+    q.enqueue(10);
+    q.enqueue(20);
+    q.enqueue(30);
+    cout<<q.dequeue()<<endl;
+    return 0;
+}
